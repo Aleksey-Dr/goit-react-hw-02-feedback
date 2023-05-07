1903b8dd2ebd6d507bff6345826da00b48ed32e4
Aleksey-Dr <aprel181818@gmail.com>
2023-05-07 22:41:34 +0300

    Step 1
    1. Added "import { Component } from 'react';" to the App.jsx file.
    2. Add "export class App;" to the App.jsx.
    3. Changed "App" component to "App" component-class.
    4. Add the method render() to the "App" component-class.
    5. Add the method render() to the "App" component-class.
    6. Add a markup to the render() of "App" component-class.
    7. npm i prop-types
    8. npm i clsx

---
+++ comments.md
@@ -0,0 +1,9 @@
+07.05.2023
+1. Added "import { Component } from 'react';" to the App.jsx file.
+2. Add "export class App;" to the App.jsx.
+3. Changed "App" component to "App" component-class.
+4. Add the method render() to the "App" component-class.
+5. Add the method render() to the "App" component-class.
+6. Add a markup to the render() of "App" component-class.
+7. npm i prop-types
+8. npm i clsx
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countFeedbackGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  countFeedbackNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  countFeedbackBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  render() {
+    const { good } = this.state;
+    const { neutral } = this.state;
+    const { bad } = this.state;
+
+    const addFeedbackGood = this.countFeedbackGood;
+    const addFeedbackNeutral = this.countFeedbackNeutral;
+    const addFeedbackBad = this.countFeedbackBad;
+
+    return (
+      <div
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
+      >
+        <h2>Please leave feedback</h2>
+        <button type="button" onClick={addFeedbackGood}>Good</button>
+        <button type="button" onClick={addFeedbackNeutral}>Neutral</button>
+        <button type="button" onClick={addFeedbackBad}>Bad</button>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    );
+  }
+}
